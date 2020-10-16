28d97a4ba97cd0c1644a5a0380721450e9161cd8
osha7 <ogroetz@gmail.com>
2020-10-15 18:58:36 -0600

    products display

--- src/App.js
+++ src/App.js
@@ -2,21 +2,37 @@ import React from 'react';
 import './App.css';
 import Products from './components/Products'
 
-function App() {
-  return (
-    <div className="container">
-      <h1>Ecommerce Shopping Cart Application</h1>
-      <br />
-      <div className="row">
-        <div className="col-md-8">
-          <Products products={this.state.filteredProducts} handleAddToCart={this.handleAddToCart} />
-        </div>
-        <div className="col-md-4">
+class App extends React.Component {
 
+  state = {
+    products: [],
+    filteredProducts: []
+  }
+
+  componentDidMount = () => {
+    fetch("http://localhost:8000/products")
+    .then(res => res.json())
+    .then(data => this.setState({
+      products: data,
+      filteredProducts: data
+    }))
+  }
+  render() {
+    return (
+      <div className="container">
+        <h1>Ecommerce Shopping Cart Application</h1>
+        <br />
+        <div className="row">
+          <div className="col-md-8">
+            <Products products={this.state.filteredProducts} handleAddToCart={this.handleAddToCart} />
+          </div>
+          <div className="col-md-4">
+
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,30 @@
 import React from 'react'
+// import util from '../util'
 
 class Product extends React.Component {
     render() {
-        <div>
-
-        </div>
+        console.log(this.props.products)
+        const productItems = this.props.products.map(product => (
+            <div className="col-md-4" key={product.id}>
+                <div className="thumbnail text-center">
+                    <a href={`#${product.id}`} onClick={(e)=>this.props.handleAddToCart(e, product)} >
+                    <img src={`products/${product.sku}_2.jpg`} alt={product.title} />
+                    <p>{product.title}</p>
+                    </a>
+                    <div>
+                        <b>${product.price.toFixed(2)}</b><br />
+                        {/* <b>{util.formatCurrency(product.price)}</b><br /> */}
+                        <button className="btn btn-primary"
+                        onClick={(e)=>this.props.handleAddToCart(e, product)}>Add To Cart</button>
+                    </div>
+                </div>
+            </div>
+        ))
+        return(
+            <div className="row">
+                {productItems}
+            </div>
+        )
     }
 }
 export default Product;
